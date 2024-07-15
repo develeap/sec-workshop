b6c57bcb38b042a200dbe7fa74ce755ff3e47eef
Avital Tamir <avital.tamir@develeap.com>
2024-07-15 23:00:08 +0300

    Initial commit

---
+++ 00_kind-with-calico/calico_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml
